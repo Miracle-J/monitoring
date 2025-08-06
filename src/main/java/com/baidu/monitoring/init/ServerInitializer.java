b4042a5f5fe78d407a33aa32c5ba60480594a34e
Miracle-J <49387300+Miracle-J@users.noreply.github.com>
2025-08-06 17:36:50 +0800

    refactor: extract configs and add windows docker support

--- src/main/java/com/baidu/monitoring/init/ServerInitializer.java
+++ src/main/java/com/baidu/monitoring/init/ServerInitializer.java
@@ -1,6 +1,7 @@
 package com.baidu.monitoring.init;
 
 import com.baidu.monitoring.conf.LocalProperties;
+import com.baidu.monitoring.conf.DockerProperties;
 import static com.baidu.monitoring.util.SessionManager.*;
 
 import com.baidu.monitoring.webscoket.StatusChangeWebsocketHandler;
@@ -21,6 +22,7 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -39,14 +41,9 @@ public class ServerInitializer {
 	private static final Logger logger = LoggerFactory.getLogger(ServerInitializer.class);
 
 
-	// 容器名称
-	private static final String CONTAINER_NAME = "mycoturn";
-	// 镜像名称
-	private static final String IMAGE_NAME = "docker.1ms.run/coturn/coturn";
-	// 配置文件挂载路径
-	private static final String UE_VOLUME_BASE = "/mnt/baidu/app/";
-	private static final String CONFIG_MOUNT = UE_VOLUME_BASE+"turnserver.conf:/etc/coturn/turnserver.conf";
-	private static final String UE_IMAGE_NAME = "signalling:ue";
+
+        @Autowired
+        private DockerProperties dockerProperties;
 
 
 	// 固定线程池，用于日志输出处理（最大 8 个线程，对应 3 个 UE 的 stdout/stderr,多给两个 容错）
@@ -68,20 +65,20 @@ public class ServerInitializer {
 	public void startTurn() {
 		logger.info("初始化turn服务");
 		nowServerIp.set(localHost);
-		try {
-			runCommand(Arrays.asList("docker", "stop", CONTAINER_NAME), true);
-			runCommand(Arrays.asList("docker", "rm", CONTAINER_NAME), true);
+                try {
+                        runCommand(Arrays.asList("docker", "stop", dockerProperties.getContainerName()), true);
+                        runCommand(Arrays.asList("docker", "rm", dockerProperties.getContainerName()), true);
 
 
-			runCommand(
-				Arrays.asList("docker", "run", "-d",
-				"-p", "15050:3478",
-				"-p", "15050:3478/udp",
-				"-p", "15051-15099:15051-15099/udp",
-				"-v", CONFIG_MOUNT,
-				"--network=host",
-				"--name=" + CONTAINER_NAME,
-				IMAGE_NAME),true);
+                        runCommand(
+                                Arrays.asList("docker", "run", "-d",
+                                "-p", "15050:3478",
+                                "-p", "15050:3478/udp",
+                                "-p", "15051-15099:15051-15099/udp",
+                                "-v", dockerProperties.getConfigMount(),
+                                "--network=host",
+                                "--name=" + dockerProperties.getContainerName(),
+                                dockerProperties.getImageName()),true);
 
 			logger.info("容器状态检查：");
 			runCommand(Arrays.asList("docker", "ps", "-a"),true);
@@ -109,18 +106,18 @@ public class ServerInitializer {
 			Iterator<Integer> iterator = postSet.iterator();
 			port = iterator.next();
 			iterator.remove();
-			logger.info("启动UE服务器:"+port);
-			List<String> dockerCmd = Arrays.asList(
-				"docker", "run", "-d",
-				"--name", "signalling-" + port,
-				"-p", port + ":9416",
-				"-p", (port + 20) + ":19416",
-				"-p", (port + 30) + ":19516",
-				"-v", UE_VOLUME_BASE+"uestart.sh:/SignallingWebServer/SignallingWebServer/platform_scripts/bash/start_with_turn.sh",
-				"-v", UE_VOLUME_BASE+"common.sh:/SignallingWebServer/SignallingWebServer/platform_scripts/bash/common.sh",
-				"-v", UE_VOLUME_BASE+"config.json:/SignallingWebServer/SignallingWebServer/config.json",
-				UE_IMAGE_NAME
-			);
+                        logger.info("启动UE服务器:"+port);
+                        List<String> dockerCmd = Arrays.asList(
+                                "docker", "run", "-d",
+                                "--name", "signalling-" + port,
+                                "-p", port + ":9416",
+                                "-p", (port + 20) + ":19416",
+                                "-p", (port + 30) + ":19516",
+                                "-v", dockerProperties.getUeVolumeBase()+"uestart.sh:/SignallingWebServer/SignallingWebServer/platform_scripts/bash/start_with_turn.sh",
+                                "-v", dockerProperties.getUeVolumeBase()+"common.sh:/SignallingWebServer/SignallingWebServer/platform_scripts/bash/common.sh",
+                                "-v", dockerProperties.getUeVolumeBase()+"config.json:/SignallingWebServer/SignallingWebServer/config.json",
+                                dockerProperties.getUeImageName()
+                        );
 			runCommand(dockerCmd, true);
 			String logFileName = "./log_" + port + "_" + System.currentTimeMillis() + ".log";
 			String uePath = localProperties.getPath() + "TwinBaseGH55-Linux-Shipping";
@@ -135,8 +132,8 @@ public class ServerInitializer {
 				"-MyHttpPort=" + (port - 10),
 				"-Log=./" + logFileName
 			);
-			//不实用封装方法runCommand，不适用常驻型进程
-			Process ueProcess = new ProcessBuilder(ueCommand).start();
+                        //不使用封装方法 runCommand，不适用于常驻型进程
+                        Process ueProcess = new ProcessBuilder(buildCommand(ueCommand)).start();
 			InputStream inputStream = ueProcess.getInputStream();
 			InputStream errorStream = ueProcess.getErrorStream();
 			if (inputStream != null) consumeStream(inputStream, "[UE OUT]");
@@ -204,8 +201,8 @@ public class ServerInitializer {
 	}
 
 
-	private void runCommand(List<String> command, boolean wait) throws IOException, InterruptedException {
-		Process process = new ProcessBuilder(command).start();
+        private void runCommand(List<String> command, boolean wait) throws IOException, InterruptedException {
+                Process process = new ProcessBuilder(buildCommand(command)).start();
 
 		if (wait) {
 			int code = process.waitFor();
@@ -231,16 +228,27 @@ public class ServerInitializer {
 	}
 
 
-	private long getPidFromProcess(Process process) {
+        private long getPidFromProcess(Process process) {
 		try {
 			// Java 9+ 可直接获取 pid
 //			return process.pid();
 			Field pidField = process.getClass().getDeclaredField("pid");
 			pidField.setAccessible(true);
 			return pidField.getLong(process);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return -1;
-		}
-	}
+                } catch (Exception e) {
+                        e.printStackTrace();
+                        return -1;
+                }
+        }
+
+        private List<String> buildCommand(List<String> command) {
+                if (System.getProperty("os.name").toLowerCase().contains("win")) {
+                        List<String> cmd = new ArrayList<>();
+                        cmd.add("cmd");
+                        cmd.add("/c");
+                        cmd.addAll(command);
+                        return cmd;
+                }
+                return command;
+        }
 }
